647f8cafe9e41d9425057242617374abf9bb1921
Vadimcha <BelovBadim2014@yandex.ru>
2023-06-01 02:13:40 +0300

    Initial Commit of Twitter Clone - Add SideBar

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{ls,ts,lsx,tsx}",
+    "./pages/**/*.{ls,ts,lsx,tsx}",
+    "./components/**/*.{ls,ts,lsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
